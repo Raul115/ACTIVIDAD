6bd8efb578aadbf83c58902078ec32eb78c1ec8f
dam <dam@172.26.81.176>
2019-01-15 10:42:07 +0100

    Editado y subido por José por sexta vez

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -16,9 +16,6 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         
-        
-        
-        
         System.out.println("Esta es la rama master");
     }
     
